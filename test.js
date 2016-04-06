d2a2c655fe89b625dc063c36ccd8abc680054a0f
trebloc <ctse77@alum.berkeley.edu>
2016-04-06 15:01:26 -0700

    jquery mouseover animation

--- test.js
+++ test.js
@@ -1,13 +1,9 @@
 $(document).ready(function() {
 	console.log('JS is laoded!');
 	$('div.container-fluid').mouseover(function (){
-		$( this ).hide('slow');
-	})
-	.mouseout(function() {
-		$(this).show('slow');
-
-	});
-	
-
-
+    $(this).hide('fast');
+  })
+  .mouseleave(function() {
+    $(this).toggle('fast');
+  });
 });
